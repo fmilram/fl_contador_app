11233b984936a53f6f966ea2bd10d76480a0739c
fmilram <franmilanramos@gmail.com>
2025-10-21 10:56:54 +0200

    Añadidos botones y funciones

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,74 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int contador = 0;
+
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    const tamano30 = TextStyle(fontSize: 30);
+
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(child: const Text('Hola Mundo desde mi nuevo HomeScreen')),
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(title: Text('Contador'), elevation: 10.0),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Número de clics', style: tamano30),
+            SizedBox(height: 20),
+            Text('$contador', style: tamano30),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: CustomFloatingActionButton(
+        incrementarFn: incrementar,
+      ),
+    );
+  }
+}
+
+class CustomFloatingActionButton extends StatelessWidget {
+  final Function incrementarFn;
+
+  const CustomFloatingActionButton({super.key, required this.incrementarFn});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        //const SizedBox(width: 20),
+        FloatingActionButton(
+          //child: const Icon(Icons.add),
+          child: const Text('-1'),
+          onPressed: null,
+        ),
+        //const SizedBox(width: 20),
+        FloatingActionButton(
+          //child: const Icon(Icons.add),
+          child: const Text('RESET'),
+          onPressed: null,
+        ),
+        //const SizedBox(width: 20),
+        FloatingActionButton(
+          //child: const Icon(Icons.add),
+          child: const Text('+1'),
+          onPressed: () => incrementarFn(),
+        ),
+      ],
     );
   }
 }
